afc6c9a5540429ed04cb151f34d530d8d9940f59
vasilkovalyov <vasilkovalyov@gmail.com>
2024-02-20 22:17:41 +0200

    add api

---
+++ src/api/loadCurrencies.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+import { getFormatDate } from '../utils/date';
+
+export async function apiLoadCurrencies(date) {
+  const currentDate = date ? date : getFormatDate();
+  const response = await axios.get(
+    `https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json&date=${currentDate}`
+  );
+  return response.data;
+}
